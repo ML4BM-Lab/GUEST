7d7bfa357ca1f253725616ddcb2fd6e7f1e885c7
jesusdfc <a904346@alumni.unav.es>
2023-09-01 10:04:15 +0200

    install requires updated

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
+from pip.req import parse_requirements
 
-setup(install_requires=open('requirements.txt').read().split('\n'))
+setup(install_requires=[
+    "colorama==0.4.6",
+    "joblib==1.3.2",
+    "numpy==1.25.2",
+    "pandas==2.0.3",
+    "python-dateutil==2.8.2",
+    "pytz==2023.3",
+    "scikit-learn==1.3.0",
+    "scipy==1.11.2",
+    "six==1.16.0",
+    "threadpoolctl==3.2.0",
+    "tqdm==4.66.1",
+    "tzdata==2023.3"
+    ],
+)
